be83bb978937bf1acb590f19af55d9c3ddd314f4
Ruben Ezequiel Rojas <102380209+ezequiel-914@users.noreply.github.com>
2023-01-25 23:28:21 -0300

    consegui-hacer-funcionar-el-timer

--- script.js
+++ script.js
@@ -1,107 +1,100 @@
-
-
+var contador = 0;
 var tmp;
-var milisegundos;
+var voltas;
 
 
-function test(){
+
+function start(){
+    tmp = setInterval(timer, 1000);
+ }  
+
+function timer(){
     var tempo = document.getElementById('tempo').value;
     switch(tempo){
         case "minuto":
-            milisegundos = 5000;
+            voltas = 5;
             break;
         case "minuto-medio":
-            milisegundos = 7500;
+            voltas = 7;
             break;
         case "dois-minutos":
-            milisegundos = 10000;
+            voltas = 10;
             break;
         case "dois-medio":
-            milisegundos = 12500;
+            voltas = 12;
             break;
         case "tres-minutos":
-            milisegundos = 15000;
+             voltas = 15;
             break;
         case "tres-medio":
-            milisegundos = 17500;
+            voltas = 17;
             break;
-        case "quatro-minutos":
-            milisegundos = 20000;
+        case "quatro":
+            voltas = 20;
             break;
         case "quatro-medio":
-            milisegundos = 22500;
+            voltas = 22;
             break;
         case "cinco-minutos":
-            milisegundos = 25000;
+            voltas = 25;
             break;
     }
-    console.log(typeof(milisegundos));
-    console.log(milisegundos);
-    
+    console.log(voltas);
     document.getElementById('header').style.display="none";
     document.getElementById('clock').style.display="flex";
-}
-function start(){
-    tmp = setInterval(timer, 1000);
-}
-var contador = 0;
-function timer(){
-    
     contador++;
-    if(contador==1){
+    
+    if(contador<=(voltas*1)){
         document.getElementById('cor12').style.backgroundColor= "var(--cor12closed)";
         document.getElementById('cor1').style.backgroundColor= "var(--cor1open)";
         document.getElementById('clock-center').style.backgroundColor= "var(--cor1open)";
-    } else if(contador==2){
+    } else if(contador<=(voltas*2)){
         document.getElementById('cor1').style.backgroundColor= "var(--cor1closed)";
         document.getElementById('cor2').style.backgroundColor= "var(--cor2open)";
         document.getElementById('clock-center').style.backgroundColor= "var(--cor2open)";
-    } else if(contador==3){
+    } else if(contador<=(voltas*3)){
         document.getElementById('cor2').style.backgroundColor= "var(--cor2closed)";
         document.getElementById('cor3').style.backgroundColor= "var(--cor3open)";
         document.getElementById('clock-center').style.backgroundColor= "var(--cor3open)";
-    } else if(contador==4){
+    } else if(contador<=(voltas*4)){
         document.getElementById('cor3').style.backgroundColor= "var(--cor3closed)";
         document.getElementById('cor4').style.backgroundColor= "var(--cor4open)";
         document.getElementById('clock-center').style.backgroundColor= "var(--cor4open)";
-    } else if(contador==5){
+    } else if(contador<=(voltas*5)){
         document.getElementById('cor4').style.backgroundColor= "var(--cor4closed)";
         document.getElementById('cor5').style.backgroundColor= "var(--cor5open)";
         document.getElementById('clock-center').style.backgroundColor= "var(--cor5open)";
-    } else if(contador==6){
+    } else if(contador<=(voltas*6)){
         document.getElementById('cor5').style.backgroundColor= "var(--cor5closed)";
         document.getElementById('cor6').style.backgroundColor= "var(--cor6open)";
         document.getElementById('clock-center').style.backgroundColor= "var(--cor6open)";
-    } else if(contador==7){
+    } else if(contador<=(voltas*7)){
         document.getElementById('cor6').style.backgroundColor= "var(--cor6closed)";
         document.getElementById('cor7').style.backgroundColor= "var(--cor7open)";
         document.getElementById('clock-center').style.backgroundColor= "var(--cor7open)";
-    } else if(contador==8){
+    } else if(contador<=(voltas*8)){
         document.getElementById('cor7').style.backgroundColor= "var(--cor7closed)";
         document.getElementById('cor8').style.backgroundColor= "var(--cor8open)";
         document.getElementById('clock-center').style.backgroundColor= "var(--cor8open)";
-    } else if(contador==9){
+    } else if(contador<=(voltas*9)){
         document.getElementById('cor8').style.backgroundColor= "var(--cor8closed)";
         document.getElementById('cor9').style.backgroundColor= "var(--cor9open)";
         document.getElementById('clock-center').style.backgroundColor= "var(--cor9open)";
-    } else if(contador==10){
+    } else if(contador<=(voltas*10)){
         document.getElementById('cor9').style.backgroundColor= "var(--cor9closed)";
         document.getElementById('cor10').style.backgroundColor= "var(--cor10open)";
         document.getElementById('clock-center').style.backgroundColor= "var(--cor10open)";
-    } else if(contador==11){
+    } else if(contador<=(voltas*11)){
         document.getElementById('cor10').style.backgroundColor= "var(--cor10closed)";
         document.getElementById('cor11').style.backgroundColor= "var(--cor11open)";
         document.getElementById('clock-center').style.backgroundColor= "var(--cor11open)";
-    } else {
+    } else if(contador<=(voltas*12)){
         document.getElementById('cor11').style.backgroundColor= "var(--cor11closed)";
         document.getElementById('cor12').style.backgroundColor= "var(--cor12open)";
         document.getElementById('clock-center').style.backgroundColor= "var(--cor12open)";
-
-        contador=0;
-    }
-    
-    
+       
+} else{
+         contador=0;
 }
 
-
-
+}
